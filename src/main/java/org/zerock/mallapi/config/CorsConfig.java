6e79e43abcadd511942ac82a91f4976460de96aa
parkhaemin <lucidme@naver.com>
2026-06-03 00:15:12 +0900

    Fix: SecurityConfig 삭제 및 빌드 캐시 초기화

--- src/main/java/org/zerock/mallapi/config/CorsConfig.java
+++ src/main/java/org/zerock/mallapi/config/CorsConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -46,22 +44,9 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
 
         return source;
+        
     }
     
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "https://parkhaemin.github.io"
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+   
+    
 }
