1bc78bb73e442341316833bbf809795c410b643a
parkhaemin <lucidme@naver.com>
2026-06-03 02:07:52 +0900

    add home controller

--- src/main/java/org/zerock/mallapi/controller/ReplyController.java
+++ src/main/java/org/zerock/mallapi/controller/ReplyController.java
@@ -23,6 +23,11 @@ public class ReplyController {
 
     private final ReplyService replyService;
 
+    @GetMapping("/")
+    public String home() {
+        return "OK";
+    }
+    
     @GetMapping("/{qnaId}")
     public List<Reply> list(@PathVariable Long qnaId) {
         return replyService.list(qnaId);
