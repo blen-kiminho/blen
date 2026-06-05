51c0c5cd8e86a563957d5631fd7e86672fffa705
parkhaemin <lucidme@naver.com>
2026-06-05 16:24:44 +0900

    WebConfig addallows 수정

--- src/main/java/org/zerock/mallapi/config/WebConfig.java
+++ src/main/java/org/zerock/mallapi/config/WebConfig.java
@@ -6,11 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // /api로 시작하는 모든 경로 허용
-                .allowedOrigins("https://parkhaemin.github.io") // 허용할 프론트엔드 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+        registry.addMapping("/**")
+                // allowedOrigins("*") 대신 allowedOriginPatterns("*")를 사용합니다.
+                .allowedOriginPatterns("*") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
