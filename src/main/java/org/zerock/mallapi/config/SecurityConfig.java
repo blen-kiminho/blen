ca130f0ff7d45590f6682fed15d8405a747c4bc8
parkhaemin <lucidme@naver.com>
2026-06-05 16:35:23 +0900

    allowsoriginpatterns로 변경

--- src/main/java/org/zerock/mallapi/config/SecurityConfig.java
+++ src/main/java/org/zerock/mallapi/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package org.zerock.mallapi.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,8 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -37,11 +38,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of(
-            "http://localhost:3000",
-            "https://parkhaemin.github.io",
-            "https://*.cloudtype.app"
-        ));
+        config.setAllowedOriginPatterns(Arrays.asList("*")); // allowedOrigins("*") 대신 allowedOriginPatterns("*")를 사용합니다.   
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
